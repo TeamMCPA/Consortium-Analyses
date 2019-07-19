74cac88a3835d6e4f9b6bb6c141c99b650efcccf
bzinszer <bzinszer@gmail.com>
2019-07-18 22:11:26 -0400

    support for dir-search and search-results-struct as inputs

--- prep_nirsfiles_mcp.m
+++ prep_nirsfiles_mcp.m
@@ -1,7 +1,18 @@
 function [nirs_files_sorted, subject_ids] = prep_nirsfiles_mcp(nirs_files_list,delim_string,subj_string)
 
+% If nirs_files_list is a string, assume it's a searchstring for dir()
+if isstring(nirs_files_list) || ischar(nirs_files_list)
+    nirs_files_list = dir(nirs_files_list);
+end
+
+% If nirs_files_list is a struct (like the output of dir search) then convert
+if isstruct(nirs_files_list)
+    nirs_files_list = arrayfun(@(x) [x.folder filesep x.name],nirs_files_list,'UniformOutput',false);
+end
+
 % Determine which subjects each file belongs with
-file_names_parsed = cellfun(@(x) strsplit(x,delim_string),nirs_files_list,'UniformOutput',false);
+[~, file_names, ~] = cellfun(@fileparts, nirs_files_list,'UniformOutput',false);
+file_names_parsed = cellfun(@(x) strsplit(x,delim_string),file_names,'UniformOutput',false);
 subj_list = cellfun(@(x) x{contains(x,subj_string)},file_names_parsed,'UniformOutput',false);
 
 
