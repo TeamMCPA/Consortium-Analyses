1cf9b45fd7e67e5880081a4fb6f8b9a4c5ca680b
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2019-10-08 09:32:39 -0400

    Update split_data.m

--- prelim_scripts/split_data.m
+++ prelim_scripts/split_data.m
@@ -1,27 +1,27 @@
-function [group_dat, group_labs, test_dat, test_labs] = split_data(group_dat, group_labs, test_dat, test_labs, num_cond, condition_flags, parsed_input, summed_mcpa, g_subvec, sub_idx)
-
-for cond_idx = 1:num_cond
-    if ischar(parsed_input.Results.conditions{cond_idx}) || isstring(parsed_input.Results.conditions{cond_idx}) || iscellstr(parsed_input.Results.conditions{cond_idx})
-        [~, ~, condition_flags{cond_idx}] = intersect(parsed_input.Results.conditions{cond_idx},summed_mcpa.event_types);
-    else
-        condition_flags{cond_idx} = parsed_input.Results.conditions{cond_idx};
-    end
+function [group_data, group_labels, subj_data, subj_labels] = split_data(s_idx, cond_flags, p, n_cond, mcpa_summ,group_subvec, group_data, group_labels, subj_data, subj_labels)
+	for cond_idx = 1:n_cond
+        if ischar(p.Results.conditions{cond_idx}) || isstring(p.Results.conditions{cond_idx}) || iscellstr(p.Results.conditions{cond_idx})
+            [~, ~, cond_flags{cond_idx}] = intersect(p.Results.conditions{cond_idx},mcpa_summ.event_types);
+        else
+            cond_flags{cond_idx} = p.Results.conditions{cond_idx};
+        end
             
-    % Extract training data
-    % group_data_tmp averages across all matching triggers for a
-    % condition and outputs a subj-x-chan matrix
-	group_data_tmp = squeeze(mean(summed_mcpa.patterns(condition_flags{cond_idx},parsed_input.Results.incl_channels,g_subvec),1))';
-	group_labels_tmp = repmat(cellstr(strjoin(string(parsed_input.Results.conditions{cond_idx}),'+')),size(group_data_tmp,1),1);
-	group_dat = [ group_dat; group_data_tmp ];
-	group_labs = [ group_labs; group_labels_tmp ];
+	% Extract training data
+	% group_data_tmp averages across all matching triggers for a
+	% condition and outputs a subj-x-chan matrix
+	group_data_tmp = squeeze(mean(mcpa_summ.patterns(cond_flags{cond_idx},p.Results.incl_channels,group_subvec),1))';
+	group_labels_tmp = repmat(cellstr(strjoin(string(p.Results.conditions{cond_idx}),'+')),size(group_data_tmp,1),1);
+	group_data = [ group_data; group_data_tmp ];
+	group_labels = [ group_labels; group_labels_tmp ];
             
 	% Extract test data
-	subj_data_tmp = summed_mcpa.patterns(condition_flags{cond_idx},parsed_input.Results.incl_channels,sub_idx);
-	subj_labels_tmp = repmat(cellstr(strjoin(string(parsed_input.Results.conditions{cond_idx}),'+')),size(subj_data_tmp,1),1);
-	test_dat = [ test_dat; subj_data_tmp ];
-	test_labs = [ test_labs; subj_labels_tmp ];
+	subj_data_tmp = mcpa_summ.patterns(cond_flags{cond_idx},p.Results.incl_channels,s_idx);
+	subj_labels_tmp = repmat(cellstr(strjoin(string(p.Results.conditions{cond_idx}),'+')),size(subj_data_tmp,1),1);
+	subj_data = [ subj_data; subj_data_tmp ];
+	subj_labels = [ subj_labels; subj_labels_tmp ];
+    end
 end
-end
+
 
 
 
