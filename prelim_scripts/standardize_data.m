a17fce200b70a3ea3a4f14a721f8f34b4dd68269
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2019-10-25 09:36:10 -0400

    Delete standardize_data.m

--- prelim_scripts/standardize_data.m
+++
@@ -1,8 +0,0 @@
-function [group_dat, test_dat] = standardize_data(group, test)
-
-average = mean(group);
-sd = std(group);
-group_dat = (group-average) ./ sd;
-test_dat = (test - average) ./ sd;
-
-end
