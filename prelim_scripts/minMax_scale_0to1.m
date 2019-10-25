a11af54b8a03dd5ee58364d2b5240698e779371c
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2019-10-25 09:35:57 -0400

    Delete minMax_scale_0to1.m

--- prelim_scripts/minMax_scale_0to1.m
+++
@@ -1,9 +0,0 @@
-function [group_dat, test_dat] = normalize_data(group, test)
-
-mins = min(group);
-maxes = max(group);
-group_dat = (group - mins) ./ (maxes - mins);
-test_dat = (test-mins) ./ (maxes-mins);
-
-
-end
