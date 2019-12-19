32e936c370b96a3b53d0524943f86621f766ce4f
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2019-12-19 14:03:43 -0500

    Add files via upload

--- summarize_MCPA_Struct.m
+++ summarize_MCPA_Struct.m
@@ -1,41 +1,67 @@
-function summarized_MCPA_struct = summarize_MCPA_Struct(function_name,MCPA_struct, dimension)
+function summarized_MCPA_struct = summarize_MCPA_Struct(summary_function,MCPA_struct, averaging_dimensions, within_sub, mcp_struct)
 %SUMMARIZE_MCPA_STRUCT Convert an MCPA struct of windowed data to
 %multivariate patterns using any summarizing function, such as nanmean.
-%
-%The function is called with the following arguments:
-%function name: either a function handle (e.g., @nanmean) or a character
-%array containing the function name (e.g., 'nanmean').
-%MCPA_struct: An MCPA struct containing a pattern matrix.
-%dimension: Which dimension of the pattern matrix to operate over.
-%Available dimensions are (time x event_type x channel x subject). If the
-%dimension field is left blank, it will default to the first dimension.
-%
-%The function will return a new MCPA struct whose field patterns are
-%summarized by the specified method.
-%
-% Chengyu Deng & Benjamin Zinszer 5 may 2017
-% revised bdz 26 oct 2018
+% Fold_type_functions: can be 
 
-if ~exist('dimension','var'), dimension = 1; end
+if isempty(averaging_dimensions), averaging_dimensions = {'time'}; end
 
-if ischar(function_name)
-    function_name = str2func(function_name);
-elseif iscell(function_name)
-    function_name = str2func(function_name{:});
+if ischar(summary_function)
+    summary_function = str2func(summary_function);
+elseif iscell(summary_function)
+    summary_function = str2func(summary_function{:});
 end
+
+%% get session dimension if this is within subject decoding
+if within_sub
+    dat_to_summ = add_session_dimension(mcp_struct,MCPA_struct);
+else
+    dat_to_summ = MCPA_struct.patterns;
+end
+
 
 %% Get summarized:
-try
-    summarized_MCPA_struct_pattern = squeeze(function_name(MCPA_struct.patterns,dimension));
-catch
-    error('Failed to successfully summarize MCPA_struct to a desired form.');
+
+for dimension = 1:length(averaging_dimensions)
+    if dimension == 1
+        if strcmp(averaging_dimensions{dimension},'instance')
+           try
+                summarized_MCPA_struct_pattern = summary_function(dat_to_summ,4);
+           catch
+                error('Failed to successfully summarize MCPA_struct to a desired form.');
+           end      
+        elseif strcmp(averaging_dimensions{dimension},'time')
+            try
+                summarized_MCPA_struct_pattern = summary_function(dat_to_summ,1);
+            catch
+                error('Failed to successfully summarize MCPA_struct to a desired form.');
+            end
+        end
+    else
+        if strcmp(averaging_dimensions{dimension},'instance')
+           try
+                 summarized_MCPA_struct_pattern = summary_function(summarized_MCPA_struct_pattern,4);
+            catch
+                error('Failed to successfully summarize MCPA_struct to a desired form.');
+            end      
+        elseif strcmp(averaging_dimensions{dimension},'time')
+            try
+                summarized_MCPA_struct_pattern = summary_function(summarized_MCPA_struct_pattern,1);
+            catch
+                error('Failed to successfully summarize MCPA_struct to a desired form.');
+            end
+        end
+    end
 end
+
+
+summarized_MCPA_struct_pattern = squeeze(summarized_MCPA_struct_pattern);
+
 
 %% Return the new struct
 try
     summarized_MCPA_struct = MCPA_struct;
     summarized_MCPA_struct.created = datestr(now);
-    summarized_MCPA_struct.summarizing_function = function_name;
+    summarized_MCPA_struct.summarizing_function = summary_function;
     summarized_MCPA_struct.patterns = summarized_MCPA_struct_pattern;
     %fprintf(' Done.\n');
     
