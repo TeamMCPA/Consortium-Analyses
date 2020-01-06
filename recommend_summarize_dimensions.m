9d2a42547f271cd3b7d454ef5bbfa048de78040e
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2020-01-06 10:00:27 -0500

    recommend dimensions to summarize over

---
+++ recommend_summarize_dimensions.m
@@ -0,0 +1,27 @@
+function summarize_dimensions = recommend_summarize_dimensions(results_struct, isWithinSubjects)
+%% this function recommends dimensions for different methods of classification
+% input:
+% results_struct - input into our cross validation function. We use this to
+% determine what kind of classification the user wants
+% isWithinSubjects - marks whether the user is doing within subjects cross
+% validation. This would call for different recommended dimension than
+% between subjects cross validation
+
+% output: a list of the recommended dimensions to summarize over
+
+if strcmp(func2str(results_struct.test_handle),'mcpa_classify')
+    if isWithinSubjects
+        summarize_dimensions = {'repetition', 'time'}; 
+    else
+        summarize_dimensions = {'repetitionXsession', 'repetition+session', 'time'};
+    end    
+elseif strcmp(func2str(results_struct.test_handle),'pairwise_rsa_test')
+    summarize_dimensions = {'repetition', 'time'};      
+else
+    if isWithinSubjects
+        summarize_dimensions = {'time'};
+    else
+        summarize_dimensions = {'repetitionXsession', 'time'};
+    end     
+end
+end
