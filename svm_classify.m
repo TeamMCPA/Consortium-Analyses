6994f3fedbcac0ffaa687dd072947ccab80a2174
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2020-01-30 13:56:29 -0500

    update for pairwise

--- svm_classify.m
+++ svm_classify.m
@@ -1,35 +1,68 @@
-function classification = svm_classify(train_data, train_labels, test_data, test_labels, opts)
-%% svm_classify implements support vector machine 
+function [classification, comparisons] = svm_classify(train_data, train_labels, test_data, test_labels, opts)
+% svm_classify implements support vector machine 
 % input: model_data - data from the subjects in the group model
 %        test_data -  labels for the subjects in the group model
 %        test_data - subject data we want to predict for
 %        test_labels - subject labels, unused
 %        opts (optional) - an options struct
 
-%% create SVM model based on opts struct
-
-% check to see if we need to call fitcecoc or fitcsvm
-if isfield(opts, 'Multiclass')
-    multiclass = opts.Multiclass;
-    opts = rmfield(opts,'Multiclass');
+%% determine if we use pairwise comparisons 
+if ~exist('opts','var') || ~isfield(opts, 'pairwise') || isempty(opts)
+    opts.pairwise = false;
 else
-    multiclass = false;
+    pairwise = opts.pairwise;
+    opts = rmfield(opts,'pairwise');
 end
 
-% parse training parameters
+%% parse out the classification parameters
 input = parse_opts(opts);
 
-% build model
-if multiclass
-    t = templateSVM(input{:});
-    svm_model = fitcecoc(train_data, train_labels,'Learners', t);
+%% if pairwise classification
+if pairwise
+    
+    % set up some parameters for our compairison loop
+    number_classes = length(unique(train_labels));
+    class_names = unique(train_labels);
+    list_of_comparisons = combnk([1:number_classes],2);
+    number_of_comparisons = size(list_of_comparisons,1);
+    results_of_comparisons = nan(number_of_comparisons,1);
+    
+    for this_comp = 1:number_of_comparisons
+        
+        % figure out what classes we need to keep for this round of
+        % comparisons
+        test_classes = list_of_comparisons(this_comp,:);
+        test_class_names = class_names(test_classes);
+        
+        % select just the rows of the training data and labels that we will
+        % need for this comparison
+        train_dat = [train_data(strcmp(train_labels,test_class_names(1)),:);...
+                     train_data(strcmp(train_labels,test_class_names(2)),:)];
+        train_labs = [train_labels(strcmp(train_labels,test_class_names(1)));...
+                      train_labels(strcmp(train_labels,test_class_names(2)))];
+        
+        
+        % select just the rows of the test data and labels that we will
+        % need for this comparison
+        test_dat = [test_data(strcmp(test_labels,test_class_names(1)),:);...
+                    test_labels(strcmp(test_labels,test_class_names(2)),:)];
+        test_labs = [test_labels(strcmp(test_labels,test_class_names(1)));...
+                     test_labels(strcmp(test_labels,test_class_names(2)))];
+        
+        t = templateSVM(input{:});
+        svm_model = fitcecoc(train_dat, train_labs,'Learners', t);
+        
+        classification = predict(svm_model, test_dat);
+        
+        results_of_comparisons(:,1, this_comp) = classification;
+        results_of_comparisons(:,2, this_comp) = test_labs;  
+    end
+    comparisons = list_of_comparisons;
 else
-    svm_model = fitcsvm(train_data, train_labels, input{:});
+    t = templateSVM(input{:});
+    svm_model = fitcecoc(train_data, train_labels, 'Learners', t);
+    classification = predict(svm_model, test_data);
+    comparisons = test_labels;
 end
-
-
-
-% classify the participant
-classification = predict(svm_model, test_data);
 
 end
