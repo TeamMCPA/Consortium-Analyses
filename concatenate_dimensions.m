c614f2c6c30abdbb4739b991646681f28182965a
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2020-02-06 10:58:43 -0500

    added comments

--- concatenate_dimensions.m
+++ concatenate_dimensions.m
@@ -28,22 +28,21 @@ patterns = permute(patterns,[keep_dims, concat_dims]);
 new_patterns = reshape(patterns,[old_pattern_size(keep_dims),new_dimension_size]);
 
 % put back in proper order
-rearranged_dims = [keep_dims, concat_dims];
+rearranged_dims = [keep_dims, concat_dims]; % this is what the dimensions currently look like - all concatenated dims are at the end
 
-amount_removed_dims = length(old_pattern_size) - ndims(new_patterns);
+amount_removed_dims = length(old_pattern_size) - ndims(new_patterns); % find how many dimensions we removed by looking at difference between the dimensions
 
-rearranged_dims((end-amount_removed_dims+1):end) = [];
+rearranged_dims((end-amount_removed_dims+1):end) = []; % rearranged_dims still has whatever dimensions we removed, so we remove those from our record keeping 
 
 if max(rearranged_dims) > length(rearranged_dims)
-    [~, idx] = max(rearranged_dims);
-    rearranged_dims(idx) = rearranged_dims(idx) - amount_removed_dims;
+    [~, idx] = max(rearranged_dims); % the maximum dimension is no longer the true max. meaning if we had 6 dimensions and went down to 5, we're still counting that 6th dimension as 6 and not 5
+    rearranged_dims(idx) = rearranged_dims(idx) - amount_removed_dims; % so we subtract
 end
 
-mapping = [rearranged_dims; 1:length(rearranged_dims)];
-[temp, order] = sort(mapping(1,:));
-sorted_dims = mapping(:,order);
+mapping = [rearranged_dims; 1:length(rearranged_dims)]; % this creates a map between what we think of as the rearranged dimensions and how matlab sees the rearranged dimensions (i.e. we see it as 1 2 3 5 4, matlab sees 1 2 3 4 5)
+[temp, order] = sort(mapping(1,:)); % sort out the rearranged dims so they are in increasing order
+sorted_dims = mapping(:,order); % use this rearrangement to tell matlab how to reshape our dimensions     
 
-new_patterns = permute(new_patterns, sorted_dims(2,:));
+new_patterns = permute(new_patterns, sorted_dims(2,:)); % reshape
     
 end
-
