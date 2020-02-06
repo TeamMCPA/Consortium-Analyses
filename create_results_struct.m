21d17850d7ee304390c578f1e92dc510824d87c1
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2020-02-06 14:05:07 -0500

    update to current version

--- create_results_struct.m
+++ create_results_struct.m
@@ -1,4 +1,4 @@
-function allsubj_results = create_results_struct(within_subject, summed_mcpa, parsed_input, all_sets, num_subj, num_sets, num_feature, num_cond)
+function allsubj_results = create_results_struct(within_subject, summed_mcpa, parsed_input, all_sets, num_subj, num_sets, num_feature, num_cond, final_dimensions)
 %% create a struct to store classification results. 
 % This will be later used in permutation testing 
 
@@ -8,7 +8,7 @@ allsubj_results.summed_mcpa_patterns = summed_mcpa.patterns;
 allsubj_results.summarize_dimensions = summed_mcpa.summarize_dimensions;
 allsubj_results.subsets = all_sets;
 allsubj_results.test_handle = parsed_input.Results.test_handle;
-allsubj_results.incl_features = parsed_input.Results.incl_features;
+allsubj_results.incl_channels = parsed_input.Results.incl_channels;
 allsubj_results.conditions = parsed_input.Results.conditions;
 allsubj_results.incl_subj = parsed_input.Results.incl_subjects;
 allsubj_results.verbose = parsed_input.Results.verbose;
@@ -19,13 +19,9 @@ allsubj_results.dimensions = summed_mcpa.dimensions;
 allsubj_results.opts_struct = parsed_input.Results.opts_struct;
 
 %% get max sessions completed for later accuracy struct
-max_sessions_completed = [];
-for participant = 1:length(MCP_dat)
-    max_sessions_completed = [max_sessions_completed length(MCP_dat(participant).Experiment.Runs)];
-end
-max_sessions = max(max_sessions_completed);
-
-allsubj_results.max_sessions = max_sessions;
+num_dims = ndims(summed_mcpa.patterns);
+session_dim = num_dims -1;
+allsubj_results.max_sessions = size(summed_mcpa.patterns, session_dim);
 
 %% create accuracy struct
 for cond_id = 1:num_cond % now create place holders for decoding accuracies 
