7addd85b569c2f23116d286c7f99d1b9d73547ea
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2020-02-06 14:36:06 -0500

    Update nfold_classify_ParticipantLevel.m

--- nfold_classify_ParticipantLevel.m
+++ nfold_classify_ParticipantLevel.m
@@ -19,14 +19,14 @@ function allsubj_results = nfold_classify_ParticipantLevel(MCP_struct,varargin)
 % Arguments:
 % MCP_struct: either an MCP-formatted struct or the path to a Matlab file
 % (.mat or .mcp) containing the MCP_struct.
-% incl_channels: channels to include in the analysis. Default: all channels
+% incl_features: features to include in the analysis. Default: all features
 % incl_subjects: index of participants to include. Default: all participants
 % baseline_window: [onset, offset] in seconds. Default [-5,0]
 % time_window: [onset, offset] in seconds. Default [2,6]
 % conditions: cell array of condition names / trigger #s. Default: {1,2}
 % summary_handle: function handle (or char of function name) to specify how
-% time-x-channel data should be summarized into features. Default: nanmean
-% setsize: number of channels to analyze (for subset analyses) Default: all
+% time-x-feature data should be summarized into features. Default: nanmean
+% setsize: number of features to analyze (for subset analyses) Default: all
 % test_handle: function handle for classifier. Default: mcpa_classify
 % opts_struct: contains additional classifier options. Default: empty struct
 % verbose: logical flag to report status updates and results. Default: true
@@ -52,16 +52,16 @@ end
 
 
 %% Parse out the input data
-p = parse_inputs(MCP_struct, varargin{:});
+p = parse_inputs(MCP_struct, varargin{:});   
 
-%% Setting up the combinations of channel subsets
+%% Setting up the combinations of feature subsets
 % Create all possible subsets. If setsize is equal to the total number of
-% channels, there will only be one 'subset' which is the full channel
-% array. If setsize is less than the total number of channels, there will
+% features, there will only be one 'subset' which is the full feature
+% array. If setsize is less than the total number of features, there will
 % be n-choose-k subsets to analyze.
 %
 % The size of the subsets can grow extremely quickly with the size of
-% incl_channels. Consequently, there is a default max of 1000000 sets,
+% incl_features. Consequently, there is a default max of 1000000 sets,
 % which can be customized. If the total number of sets is larger than the
 % max number of allowed sets, the list of sets will be subsampled.
 
@@ -82,14 +82,14 @@ end
 
 mcpa_struct = MCP_to_MCPA(MCP_struct,...
     p.Results.incl_subjects,...
-    p.Results.incl_channels,...
+    p.Results.incl_features,...
     p.Results.time_window,...
     p.Results.baseline_window);
 
 %% summarize MCPA struct
 % Step 2: Apply the desired function (e.g., @nanmean) for summarizing time
 % window data. You can write custom functions to deal with time- and
-% channel-domain data however you want. Default behavior is to apply the
+% feature-domain data however you want. Default behavior is to apply the
 % function along the first dimension of the MCPA pattern (instance) and then the second dimension (time),
 % but this can also be changed.
 
@@ -144,7 +144,7 @@ end
 %% Prep some basic parameters
 n_subj = length(p.Results.incl_subjects);
 n_sets = size(sets,1);
-n_chan = length(p.Results.incl_channels);
+n_feature = length(p.Results.incl_features);
 try n_cond = length(unique(p.Results.conditions)); catch, n_cond = length(p.Results.conditions); end
 
 
@@ -156,8 +156,9 @@ allsubj_results = create_results_struct(false,...
     sets,...
     n_subj,...
     n_sets,...
-    n_chan,...
-    n_cond);
+    n_feature,...
+    n_cond,...
+    final_dimensions);
 
 stack = dbstack;
 current_folding_function = stack.name;
@@ -171,20 +172,19 @@ for s_idx = 1:n_subj
     end
     tic;
     
-    %% Run over channel subsets
-    temp_set_results_cond = nan(n_cond,n_sets,n_chan);
+    %% Run over feature subsets
+    temp_set_results_cond = nan(n_cond,n_sets,n_feature);
     
     %% Folding & Dispatcher: Here's the important part
     % Right now, the data have to be treated differently for 2
     % conditions vs. many conditions. In MCPA this is because 2
-    % conditions can only be compared in channel space (or, hopefully,
+    % conditions can only be compared in feature space (or, hopefully,
     % MNI space some day). If there are a sufficient number of
-    % conditions (6ish or more), we abstract away from channel space
+    % conditions (6ish or more), we abstract away from feature space
     % using RSA methods. Then classifier is trained/tested on the RSA
     % structures. This works for our previous MCPA studies, but might
     % not be appropriate for other classifiers (like SVM).
-    
-    
+       
     [group_data, group_labels, subj_data, subj_labels] = split_test_and_train(s_idx,...
         p.Results.conditions,...
         mcpa_summ.patterns,...
@@ -203,39 +203,46 @@ for s_idx = 1:n_subj
                 fprintf(' .')
             end
         end
-        % Select the channels for this subset
-        set_chans = sets(set_idx,:);
+        % Select the features for this subset
+        set_features = sets(set_idx,:);
 
         %% classify
         % call differently based on if we do RSA or not
+        % if we do pairwise comparison, the result test_labels will be a 3d
+        % matrix with the dimensions: predicted label x correct label x
+        % index of comparison. The output 'comparisons' will be the
+        % conditions that were compared and can either be a 2d cell array or a
+        % matrix of integers. If we don't do pairwise comparisons, the
+        % output 'test_labels' will be a 1d cell array of predicted labels.
+        % The output 'comparisons' will be a 1d array of the correct
+        % labels.
         
         % RSA
         if strcmp(func2str(p.Results.test_handle),'pairwise_rsa_test')
             [test_labels, comparisons] = p.Results.test_handle(...
-                group_data(:,set_chans,:), ...
+                group_data(:,set_features,:), ...
                 group_labels,...
-                subj_data(:,set_chans,:),...
+                subj_data(:,set_features,:),...
                 subj_labels,...
                 p.Results.opts_struct);
 
         else
             [test_labels, comparisons] = p.Results.test_handle(...
-                    group_data(:,set_chans), ...
-                    group_labels,...
-                    subj_data(:,set_chans),...
-                    subj_labels,...
-                    p.Results.opts_struct);
+                group_data(:,set_features), ...
+                group_labels,...
+                subj_data(:,set_features),...
+                subj_labels,...
+                p.Results.opts_struct);
         end
-
            
-       %% Record results 
+       %% Record results .
         if size(test_labels,2) > 1 % test labels will be a column vector if we don't do pairwise
             if s_idx==1 && set_idx == 1, allsubj_results.accuracy_matrix = nan(n_cond,n_cond,min(n_sets,p.Results.max_sets),n_subj); end
 
             if iscell(comparisons)
                 subj_acc = nanmean(strcmp(test_labels(:,1,:), test_labels(:,2,:)));
-                comparisons = cellfun(@(x) find(strcmp(x,mcpa_summ.event_types)),comparisons); 
-            else
+                comparisons = cellfun(@(x) find(strcmp(x,mcpa_summ.event_types)),comparisons);
+            else  
                 subj_acc = nanmean(strcmp(test_labels(:,1,:), test_labels(:,2,:)));
             end
 
@@ -250,13 +257,14 @@ for s_idx = 1:n_subj
             for cond_idx = 1:n_cond
                 temp_acc = cellfun(@strcmp,...
                 subj_labels(strcmp(strjoin(string(p.Results.conditions{cond_idx}),'+'),subj_labels)),... % known labels
-                temp_test_labels(strcmp(strjoin(string(p.Results.conditions{cond_idx}),'+'),subj_labels))...% classifier labels
+                test_labels(strcmp(strjoin(string(p.Results.conditions{cond_idx}),'+'),subj_labels))...% classifier labels
                 );
 
-                temp_set_results_cond(cond_idx,set_idx,set_chans) = nanmean(temp_acc);
-
+                temp_set_results_cond(cond_idx,set_idx,set_features) = nanmean(temp_acc);
+            end
+            for cond_idx = 1:n_cond
                 allsubj_results.accuracy(cond_idx).subsetXsubj(:,s_idx) = nanmean(temp_set_results_cond(cond_idx,:,:),3);
-                allsubj_results.accuracy(cond_idx).subjXchan(s_idx,:) = nanmean(temp_set_results_cond(cond_idx,:,:),2);
+                allsubj_results.accuracy(cond_idx).subjXfeature(s_idx,:) = nanmean(temp_set_results_cond(cond_idx,:,:),2);
             end
         end
                   
@@ -272,18 +280,18 @@ if p.Results.verbose
     if n_sets > 1 && length(p.Results.conditions)==2
         
         figure
-        errorbar(1:size(allsubj_results.accuracy(1).subjXchan,2),mean(allsubj_results.accuracy(1).subjXchan),std(allsubj_results.accuracy(1).subjXchan)/sqrt(size(allsubj_results.accuracy(1).subjXchan,1)),'r')
+        errorbar(1:size(allsubj_results.accuracy(1).subjXfeature,2),mean(allsubj_results.accuracy(1).subjXfeature),std(allsubj_results.accuracy(1).subjXfeature)/sqrt(size(allsubj_results.accuracy(1).subjXfeature,1)),'r')
         hold;
-        errorbar(1:size(allsubj_results.accuracy(2).subjXchan,2),mean(allsubj_results.accuracy(2).subjXchan),std(allsubj_results.accuracy(2).subjXchan)/sqrt(size(allsubj_results.accuracy(2).subjXchan,1)),'k')
-        title('Decoding Accuracy across all channels: Red = Cond1, Black = Cond2')
-        set(gca,'XTick',[1:length(p.Results.incl_channels)])
-        set(gca,'XTickLabel',p.Results.incl_channels)
+        errorbar(1:size(allsubj_results.accuracy(2).subjXfeature,2),mean(allsubj_results.accuracy(2).subjXfeature),std(allsubj_results.accuracy(2).subjXfeature)/sqrt(size(allsubj_results.accuracy(2).subjXfeature,1)),'k')
+        title('Decoding Accuracy across all features: Red = Cond1, Black = Cond2')
+        set(gca,'XTick',[1:length(p.Results.incl_features)])
+        set(gca,'XTickLabel',p.Results.incl_features)
         hold off;
         
         figure
-        errorbar(1:size(allsubj_results.accuracy(1).subjXchan,1),mean(allsubj_results.accuracy(1).subjXchan'),repmat(std(mean(allsubj_results.accuracy(1).subjXchan'))/sqrt(size(allsubj_results.accuracy(1).subjXchan,2)),1,size(allsubj_results.accuracy(1).subjXchan,1)),'r')
+        errorbar(1:size(allsubj_results.accuracy(1).subjXfeature,1),mean(allsubj_results.accuracy(1).subjXfeature'),repmat(std(mean(allsubj_results.accuracy(1).subjXfeature'))/sqrt(size(allsubj_results.accuracy(1).subjXfeature,2)),1,size(allsubj_results.accuracy(1).subjXfeature,1)),'r')
         hold;
-        errorbar(1:size(allsubj_results.accuracy(2).subjXchan,1),mean(allsubj_results.accuracy(2).subjXchan'),repmat(std(mean(allsubj_results.accuracy(2).subjXchan'))/sqrt(size(allsubj_results.accuracy(2).subjXchan,2)),1,size(allsubj_results.accuracy(2).subjXchan,1)),'k')
+        errorbar(1:size(allsubj_results.accuracy(2).subjXfeature,1),mean(allsubj_results.accuracy(2).subjXfeature'),repmat(std(mean(allsubj_results.accuracy(2).subjXfeature'))/sqrt(size(allsubj_results.accuracy(2).subjXfeature,2)),1,size(allsubj_results.accuracy(2).subjXfeature,1)),'k')
         title('Decoding Accuracy across all subjects: Red = Cond1, Black = Cond2')
         set(gca,'XTick',[1:p.Results.incl_subjects])
         hold off;
