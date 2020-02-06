d8fa7d2a3da62e28658371d48d756db6b570fae7
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2020-02-06 14:13:04 -0500

    Update parse_inputs.m

--- parse_inputs.m
+++ parse_inputs.m
@@ -11,7 +11,7 @@ function p = parse_inputs(MCP_struct, varargin)
 p = inputParser;
 
 % parameters used for all kinds of classifiers
-addParameter(p,'incl_features',[1:max(arrayfun(@(x) size(x.fNIRS_Data.Hb_data.Oxy,2),MCP_struct))],@isnumeric);
+addParameter(p,'incl_channels',[1:max(arrayfun(@(x) size(x.fNIRS_Data.Hb_data.Oxy,2),MCP_struct))],@isnumeric);
 addParameter(p,'incl_subjects',[1:length(MCP_struct)],@isnumeric);
 addParameter(p,'time_window',[2,6],@isnumeric);
 addParameter(p,'baseline_window',[-5 0],@isnumeric);
@@ -25,10 +25,10 @@ addParameter(p,'norm_data', false, @islogical);
 addParameter(p,'verbose',true,@islogical);
 addParameter(p, 'summarize_dimensions', {});
 addParameter(p, 'final_dimensions', {});
-addParameter(p, 'pairwise', false, @islogical);
 
 % for within subjects decoding with 1 session
 addParameter(p, 'test_percent', []);
+
 
 % parameters used if norming the data
 addParameter(p,'norm_withinSessions', true, @islogical);
