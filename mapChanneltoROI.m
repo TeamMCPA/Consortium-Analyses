594d1e8f4c75256fea09e936d21b65b021831d6c
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2020-03-10 16:00:12 -0400

    remove weighting and update parameters

--- mapChanneltoROI.m
+++ mapChanneltoROI.m
@@ -1,21 +1,25 @@
-function transformation_mat = mapChanneltoROI(n_chan, n_features, subj_idx, session_idx)
+function transformation_mat = mapChanneltoROI(n_chan, n_areas, td_path, pos)
+% n_chan = number of channels on nirs cap, not number included in analysis
+% n_areas = number of Brodmann's areas - eventually we could update this so
+% that we can also use other regions of interest, but for now this parameter
+% should always be 47
+% td_path = file path to find the TDdatabase
+% pos = POS file that contains where the probes sat for a single
+% participant's session
+
 %% create list of area names
-load('TDdatabase.mat');
+load([td_path 'TDdatabase.mat']);
 areas = {};
 for area = 1:47
     area_name = ['brodmann_area_', int2str(area)];
     areas{end+1} = area_name;
 end
 
-%% load the POS file
-pathname = ['/Users/annaph/Desktop/decoding_POS_data/dec',num2str(subj_idx,'%02.0f'),'/decoding_',num2str(session_idx),'/'];
-load([pathname 'POS.mat']);
-
 %% initialize the transformation matrix
-transformation_mat = zeros(n_chan, n_features);
+transformation_mat = zeros(n_chan, n_areas);
 
 %% then find distance for every channel
-channel_locations = R.ch.xyzC';
+channel_locations = pos.R.ch.xyzC';
 
 for chan = 1:n_chan
     for area = 1:length(areas)
@@ -30,10 +34,7 @@ for chan = 1:n_chan
     transformation_mat(chan, chan_area) = 1;
 end
 
-%% then weight by how many channels are in each area
-weight = sum(transformation_mat,1);
-transformation_mat = transformation_mat ./ weight;
-transformation_mat(isnan(transformation_mat))=0;
-
 end
+
+
 
