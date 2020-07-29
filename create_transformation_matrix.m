c6d80e5824feb5e5426886c38b3d7294fe40c6f6
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2020-07-29 11:22:05 -0400

    remove mentions to BAs, remove code specific to TDdatabase

--- create_transformation_matrix.m
+++ create_transformation_matrix.m
@@ -1,52 +1,30 @@
-function mcp_struct = create_transformation_matrix(mcp_struct, convert_to_brodmanns, split_hemispheres, pos_pathname, probe_loc_file, tdDatabase, session_idx)
+function mcp_struct = create_transformation_matrix(mcp_struct, convert_to_ROI, pos_pathname, probe_loc_file, ROIdatabase, areas, session_idx)
 %% create a transformation matrix for each subjects' sessions
 % input:
 % mcp_struct - mcp data structure for one participant
-% convert_to_brodmanns - true or false of whether we want ROI space or
+% convert_to_ROI - true or false of whether we want ROI space or
 % channel space
-% split_hemispheres - true or false indicating whether the TDDatabase needs to be split into left and right hemispheres
 % pos_pathname - path to where that participant's session probe location data can be found
 % probe_loc_file - name of file where that session's probe locations are
 % stored (usually POS.mat)
-% tdDatabase - atlas with MNI coordinates for Brodmann's areas
-% session_idx - which session we are trying to co-register
+% ROIdatabase - database with MNI coordiantes where ROIs can be found
+% areas - name of ROIs to co-register to as they appear in MNI database
+% session_idx - which session's data are we co-registering for that participant
+
+%% check that only one subject was entered at a time
+if length(mcp_struct) > 1
+    error('Only one subject can be entered into create_transformation_matrix at a time. Please specify which participant to use.')
+end
 
 %% create transformation matrices
-
 n_channels = length(mcp_struct.Experiment.Probe_arrays.Channels);
 
-if convert_to_brodmanns
+if convert_to_ROI
     
     pos_mat = load([pos_pathname probe_loc_file]); % pos file
-    areas = {};
-    for area = 1:47
-        area_name = ['brodmann_area_', int2str(area)];
-        areas{end+1} = area_name;
-    end
-    
-    if split_hemispheres
-        brodmanns_areas_hemispheres = struct;
-        
-        for a = 1:length(areas)
-            temp_area = tdDatabase.(areas{a});
-            left_area = find(temp_area(:,1) < 0);
-            right_area = find(temp_area(:,1) > 0);
-            new_name_left = [areas{a} '_left'];
-            new_name_right = [areas{a} '_right'];
-            brodmanns_areas_hemispheres.(new_name_left) = temp_area(left_area,:);
-            brodmanns_areas_hemispheres.(new_name_right) = temp_area(right_area,:);
-        end
-        
-        areas = fieldnames(brodmanns_areas_hemispheres);
-        
-        transformation_mat = mapChanneltoROI(n_channels, brodmanns_areas_hemispheres, pos_mat, areas);
-        
-    else
-        transformation_mat = mapChanneltoROI(n_channels, tdDatabase, pos_mat,areas);
-   
-    end
-else
-    
+    transformation_mat = mapChanneltoROI(n_channels, ROIdatabase, pos_mat, areas);
+
+else    
     transformation_mat = eye(n_channels);
 end
 
