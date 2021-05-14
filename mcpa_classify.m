1df3d946d644d13620daf09e33cf96f121ff1b9f
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2021-05-14 10:59:11 -0400

    clarify comments when removing empty columns

--- mcpa_classify.m
+++ mcpa_classify.m
@@ -46,55 +46,55 @@ model_dat = model_data(train_order, :);
 % section finds NaN cells in the test and train matrix, then removes
 % columns where both test and train were entirely NaN
 
-% empty_x_vals_model = [];
-% empty_y_vals_model = [];
-% for i = 1:size(model_data,4)
-%     for j = 1:size(model_data,3)
-%         [x,y] = find(isnan(model_dat(:,:,j,i)));
-%         if length(unique(x)) == size(model_dat,1) && length(unique(y)) == size(model_dat,2)
-%             continue;
-%         else
-%             empty_x_vals_model = [empty_x_vals_model; x];
-%             empty_y_vals_model = [empty_y_vals_model; y];
-%         end
-%     end      
-% end
-% empty_x_vals_model = unique(empty_x_vals_model);
-% empty_y_vals_model = unique(empty_y_vals_model);
-% 
-% 
-% empty_x_vals_test = [];
-% empty_y_vals_test = [];
-% for i = 1:size(test_data,4)
-%     for j = 1:size(test_data,3)
-%         [x,y] = find(isnan(test_data(:,:,j,i)));
-%         if length(unique(x)) == size(test_data,1) && length(unique(y)) == size(test_data,2)
-%             continue;
-%         else
-%             empty_x_vals_test = [empty_x_vals_test; x];
-%             empty_y_vals_test = [empty_y_vals_test; y];
-%         end
-%     end      
-% end
-% 
-% empty_x_vals_test = unique(empty_x_vals_test);
-% empty_y_vals_test = unique(empty_y_vals_test);
-% 
-% cols = 1:size(model_data,2);
-% rows = 1:size(model_data,1);
-% 
-% if length(empty_x_vals_model) == size(model_data,1) && length(empty_y_vals_model) ~= size(model_data,2)
-%     % remove columns
-%     remove_cols = union(empty_y_vals_model, empty_y_vals_test);
-%     keep = ~ismember(cols, remove_cols);
-%     model_dat = model_dat(:,keep', :,:);
-%     test_dat = test_dat(:, keep', :,:);   
-% elseif length(empty_x_vals_model) == size(model_data,1) && length(empty_y_vals_model) == size(model_data,2)
-%     % this is an empty session
-%     warning('There is no data in this session')
-% end
-%     
-% 
+empty_x_vals_model = [];
+empty_y_vals_model = [];
+for i = 1:size(model_data,4)
+    for j = 1:size(model_data,3)
+        [x,y] = find(isnan(model_dat(:,:,j,i)));
+        if length(unique(x)) == size(model_dat,1) && length(unique(y)) == size(model_dat,2)
+            continue;
+        else
+            empty_x_vals_model = [empty_x_vals_model; x];
+            empty_y_vals_model = [empty_y_vals_model; y];
+        end
+    end      
+end
+empty_x_vals_model = unique(empty_x_vals_model);
+empty_y_vals_model = unique(empty_y_vals_model);
+
+
+empty_x_vals_test = [];
+empty_y_vals_test = [];
+for i = 1:size(test_data,4)
+    for j = 1:size(test_data,3)
+        [x,y] = find(isnan(test_data(:,:,j,i)));
+        if length(unique(x)) == size(test_data,1) && length(unique(y)) == size(test_data,2)
+            continue;
+        else
+            empty_x_vals_test = [empty_x_vals_test; x];
+            empty_y_vals_test = [empty_y_vals_test; y];
+        end
+    end      
+end
+
+empty_x_vals_test = unique(empty_x_vals_test);
+empty_y_vals_test = unique(empty_y_vals_test);
+
+cols = 1:size(model_data,2);
+rows = 1:size(model_data,1);
+
+if length(empty_x_vals_model) == size(model_data,1) && length(empty_y_vals_model) ~= size(model_data,2)
+    % remove columns if the whole column is empty
+    remove_cols = union(empty_y_vals_model, empty_y_vals_test);
+    keep = ~ismember(cols, remove_cols);
+    model_dat = model_dat(:,keep', :,:);
+    test_dat = test_dat(:, keep', :,:);   
+elseif length(empty_x_vals_model) == size(model_data,1) && length(empty_y_vals_model) == size(model_data,2)
+    % this is an empty session
+    warning('There is no data in this session')
+end
+    
+
 
 %% Average across training data to get model features for each class
 model_patterns = nan(size(model_dat,2),length(model_classes));
