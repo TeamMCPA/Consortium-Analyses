af5d3e263c92c36bb87e4e00c7720cd81bda2d6b
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2021-06-14 11:28:53 -0400

    update help doc

--- rsa_visualize_test_and_training_matrices.m
+++ rsa_visualize_test_and_training_matrices.m
@@ -1,4 +1,12 @@
 function rsa_visualize_test_and_training_matrices(training_matrix, model_labels, test_matrix, test_labels)
+%% visualize the final test and training models
+% inputs: - training_matrix: (dis)similarity matrix for training model
+%         - model_labels: training model labels
+%         - test_matrix: (dis)similarity matrix for test model
+%         - test_labels: test model labels
+% returns: visualization of the test and train models
+
+
     figure();
     % Training data
     subplot(1,2,1)
