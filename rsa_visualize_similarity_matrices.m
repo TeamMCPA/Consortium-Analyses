950fca2e0beffbeeda8da85d677e0344647c18ff
Anna Herbolzheimer <aherbolzheimer@ucdavis.edu>
2021-06-14 11:25:59 -0400

    add in help doc

--- rsa_visualize_similarity_matrices.m
+++ rsa_visualize_similarity_matrices.m
@@ -1,4 +1,17 @@
 function rsa_visualize_similarity_matrices(model_data, test_data, model_mat, test_mat)
+%% create a visualization of the (dis)similarity matrix for each subject and each session
+% inputs: - model data: feature matrix for training data (condition x
+%                       feature x session x subject)
+%         - test_data: feature matrix for test data (condition x feature x
+%                      session x subject)
+%         - model_mat: similarity matrices for model data (condition x
+%                      condition x session x subject)
+%         - test_mat: similarity matrices for test data (condition x
+%                      condition x session x subject)
+% returns: a figure containing the (dis)similarity matrix for each subject and
+%          each session
+
+    % plot the training data
     plot_idx = 1;
     figure
  
@@ -15,6 +28,7 @@ function rsa_visualize_similarity_matrices(model_data, test_data, model_mat, tes
         end
     end
     
+    % plot the test data
     plot_idx = 1;
     figure
     for session_idx = 1:size(test_data,3)
